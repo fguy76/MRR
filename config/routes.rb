e2be218991a6ef30e259cfc50049ded92d8f309f
T Nic <us0976@gmail.com>
2014-02-01 00:11:23 +0530

    make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 MRR::Application.routes.draw do
-  resources :users
-
-
+  
+  match 'signup', to: 'users#new'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
